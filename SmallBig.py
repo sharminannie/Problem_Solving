5dc7448d6bc36d9b54b399e760a2f934642c4b81
Sharmin Sultana Annie <123810478+sharminannie@users.noreply.github.com>
2025-07-03 15:33:34 +0600

    Add files via upload

---
+++ SmallBig.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a positive number:"))
+
+if n<=100:
+    print("small")
+elif n<=1000:
+    print("medium")
+else:
+    print("big")
---
+++ name.py
@@ -0,0 +1,11 @@
+n = input("Enter a input:")
+
+print(n)
+
+if n == "Asma":
+    print("Asma Sultana Abbie")
+elif n == "Sharmin":
+    print("Sharmin Sultana Annie")
+else:
+    print("NULL")
+    
